01d444f7892eafae41713c5920873a685b263937
Arszaptr <arszaptr@gmail.com>
2026-05-12 10:24:24 +0700

    initial commit: setup frontend dan backend

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"status": "ok", "message": "Thriftchain-AG API berjalan!"})
+
+@app.route("/api/test")
+def test():
+    return jsonify({"status": "ok", "data": "Hello dari backend!"})
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
